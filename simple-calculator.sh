e98fcc64f884fd802e51e1c7e58501e77bfe35d2
Vishwanath Ashireddy <VishwanathAshireddy@gmail.com>
2022-07-02 12:12:54 +0000

    Adding a check to validate user inputs.

--- simple-calculator.sh
+++ simple-calculator.sh
@@ -4,12 +4,25 @@ set -e
 echo -e "Select your choice of operation from below:\n1) Sum"
 read operation
 
+case $operation in
+        "1") echo "Enter first number"
+                read a
+                echo "Enter second number"
+                read b
+                ;;
+        *) echo "Wrong choice"
+                ;;
+esac
+
+re='^[0-9]+$'
+if ! [[ ${a} =~ $re ]] ; then
+   echo "error: $a an integer" ; exit 1
+elif ! [[ ${b} =~ $re ]] ; then
+   echo "error: ${b} an integer" ; exit 1
+fi
+
 case $operation in 
-	1) echo "Enter first number"
-		read a
-		echo "Enter second number"
-		read b
-		sum=`expr $a + $b`
+	1) 	sum=`expr $a + $b`
 		echo "Sum of $a and $b is $sum."
 		;;
 	*) echo "Wrong choice"
